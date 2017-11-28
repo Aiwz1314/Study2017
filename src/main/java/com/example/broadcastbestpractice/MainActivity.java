b8310164272e8d58ec350e4c2dfc1be387f847f7
wei zheng <weizheng111@gmail.com>
2017-11-28 23:07:36 +0800

    第一次提交

---
+++ src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.broadcastbestpractice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //我是新添加的内容 测试git
+        //我是新添加的内容 测试git2
+        //我是新添加的内容 测试git3
+        Button forceOffline = findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
